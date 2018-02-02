c0b10bcf61619fe8cdf068d88245ed08f577aaab
Alexander Poeppel <trekie10b@gmx.de>
2018-02-02 11:24:08 +0100

    Added exemplary config file

--- console_common.bash
+++ console_common.bash
@@ -27,50 +27,58 @@ __console_parse_parameters() {
     INPUT_PARTS+=("--")
 
     for ((i=0; i<${#INPUT_PARTS[@]}; i++)); do
-        #echo "Part $i: ${INPUT_PARTS[$i]}"
+        echo "Part $i: ${INPUT_PARTS[$i]}"
         if [[ ${INPUT_PARTS[$i]} == "--" ]]; then
             break
         fi
         for option in $PARAMETER_DEFINITIONS; do
             declare -n optionArray="$option"
-            #echo "Array: ${optionArray[@]}"
-            #echo "Short Option: "${optionArray[__CONSOLE_SHORT_OPT_INDEX]}
-            #echo "Long Option: "${optionArray[__CONSOLE_LONG_OPT_INDEX]}
-            #echo "Is Flag: "${optionArray[__CONSOLE_IS_FLAG_INDEX]}
-            #echo "Single Value: "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}
-            #echo "Single Value Expanded: "${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}
-            #echo "Checking -"${optionArray[__CONSOLE_SHORT_OPT_INDEX]}" equal to ${INPUT_PARTS[$i]}"
-            #echo "Checking --"${optionArray[__CONSOLE_LONG_OPT_INDEX]}" equal to ${INPUT_PARTS[$i]}"
-            #echo "Possible parameter value would be: ${INPUT_PARTS[$((i+1))]}"
+            echo "Array: ${optionArray[@]}"
+            echo "Short Option: "${optionArray[__CONSOLE_SHORT_OPT_INDEX]}
+            echo "Long Option: "${optionArray[__CONSOLE_LONG_OPT_INDEX]}
+            echo "Is Flag: "${optionArray[__CONSOLE_IS_FLAG_INDEX]}
+            echo "Single Value: "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}
+            echo "Single Value Expanded: "${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}
+            echo "Checking -"${optionArray[__CONSOLE_SHORT_OPT_INDEX]}" equal to ${INPUT_PARTS[$i]}"
+            echo "Checking --"${optionArray[__CONSOLE_LONG_OPT_INDEX]}" equal to ${INPUT_PARTS[$i]}"
+            echo "Possible parameter value would be: ${INPUT_PARTS[$((i+1))]}"
             if [[ "-"${optionArray[__CONSOLE_SHORT_OPT_INDEX]} == ${INPUT_PARTS[$i]} || "--"${optionArray[__CONSOLE_LONG_OPT_INDEX]} == ${INPUT_PARTS[$i]} ]]; then
-                #echo "Examined character: ${INPUT_PARTS[(($i+1))]:0:1}"
-                #echo "Is Flag: ${optionArray[__CONSOLE_IS_FLAG_INDEX]}"
+                echo "Examined character: ${INPUT_PARTS[(($i+1))]:0:1}"
+                echo "Is Flag: ${optionArray[__CONSOLE_IS_FLAG_INDEX]}"
                 if [[ ${INPUT_PARTS[(($i+1))]:0:1} != "-" && ${optionArray[__CONSOLE_IS_FLAG_INDEX]} == false ]]; then
-                    #echo "Case 1"
+                    echo "Arg case"
                     # A parameter is required for the current opition, so read it and advance the pointer by 2
                     printf -v "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}" %s ""
                     while [[ ${INPUT_PARTS[(($i+1))]:0:1} != "-" ]]; do
-                        #echo "Possible parameter value would be: ${INPUT_PARTS[$((i+1))]}"
+                        echo "Possible parameter value would be: ${INPUT_PARTS[$((i+1))]}"
                         if [ -n "${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}" ]; then
                             printf -v "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}" %s "${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]} ${INPUT_PARTS[$((i+1))]}"
                         else
                             printf -v "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}" %s "${INPUT_PARTS[$((i+1))]}"
                         fi
-                        #echo "i is now: $i"
+                        echo "i is now: $i"
                         ((i++))
-                        #echo "i is now: $i"
-                        #echo "Next Possible: ${INPUT_PARTS[$((i+1))]}"
+                        echo "i is now: $i"
+                        echo "Next Possible: ${INPUT_PARTS[$((i+1))]}"
                     done
-                    #echo "Wrote follwing to variable: ${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}"
+                    echo "Wrote follwing to variable: ${!optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}"
                 elif [[ ${INPUT_PARTS[(($i+1))]:0:1} == "-" && ${optionArray[__CONSOLE_IS_FLAG_INDEX]} == true ]]; then
-                    #echo "Case 2"
+                    echo "Flag case"
                     # The current option is only a flag, so set it and advance the pointer by 1
                     printf -v "${optionArray[__CONSOLE_TARGET_VARIABLE_INDEX]}" %s "true"
                 else
-                    #echo "Case 3"
+                    echo "Error Case"
                     # An error occured, which getopt should have caught
-                    echo "A required parameter is missing. Exit."
-                    exit 3
+                    if [[ ${optionArray[__CONSOLE_IS_FLAG_INDEX]} == false ]]; then
+                        echo "A required parameter is missing. Exit."
+                        exit 3
+                    elif [[ ${optionArray[__CONSOLE_IS_FLAG_INDEX]} == true ]]; then
+                        echo "A flag was passed a parameter. Exit."
+                        exit 4
+                    else
+                        echo "An unknown error occurred. Exit."
+                        exit 5
+                    fi
                 fi
             fi
         done
---
+++ ros2_config.bash
@@ -0,0 +1,21 @@
+## Middleware (DDS) implementation to be used
+## Can be:
+## - RTPS:			FastRTPS, the default implementation bundled with ROS
+## - OSplice:		PrismTech OpenSplice
+## - RTI:			RTI Connext DDS implementation (proprietary and neccessary for realtime)
+ROS2_RMW="RTI"
+
+ROS2_BASE_DIR="~/ros2_ws/"
+
+ROS2_NO_ECHO=false
+
+## Set the setup script to be used, either:
+## - Global:		Assuming one workspace, this sets up the workspace globally
+## - Local:			Useful when using multiple ROS2 worskspaces
+ROS2_WS_SETUP="Global"
+
+ROS2_INSTALL_DIR=$ROS2_BASE_DIR"install/"
+
+RTI_LICENSE="~/rti_connext_dds-5.3.0/rti_license.dat"
+
+RTI_SCRIPT="~/rti_connext_dds-5.3.0/resource/scripts/rtisetenv_x64Linux3gcc5.4.0.bash"
